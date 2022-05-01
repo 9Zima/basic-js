1e2e10d09cf1e15dfcb59dc49f476496843f04f6
9Zima <blazingsword404@gmail.com>
2022-05-01 22:16:15 +0300

    feat: not complete simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  parts: [],
+  completeChain: "",
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.parts.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    // console.log(this.parts)
+    // if (this.parts === []) {
+    //   if (value === " ")
+    //     this.parts.push(`(${value})`)
+    //   else
+    //     this.parts.push(`( ${value} )`)
+    //   // this.completeChain += (`( ${value} )`)
+    // }
+    // else {
+    //   if (value === " ")
+    //     this.parts.push(`(${value})`)
+    //   else
+    //     this.parts.push(`( ${value} )`)
+    //   // this.completeChain += (`~~( ${value} )`)
+    // }
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!this.parts.includes(position-1) || !Number.isInteger(position) || typeof position !== "number" || position <= 0) {
+      throw Error("You can't remove incorrect link!")
+    }
+    // this.parts.splice(position-1, 1)
   },
   reverseChain() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // for (let item of this.parts) {
+    //   if (item === this.parts[0])
+    //     this.completeChain += item
+    // }
+    return this.parts.join("~~")
   }
 };
 
